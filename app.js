18f50182a1c9259da4e993318cdb8f930e0f9983
Đình Sơn FE <97991784+DinhSonFE@users.noreply.github.com>
2022-12-30 01:43:21 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,59 +1,59 @@
 const listSong = [
-    {
-        song: "Doan Xuan Ca",
-        src: "./mp3/DoanXuanCa.mp3",
-        img: "./IMG/img1.jpeg",
-    },
-    {
-        song: "Uoc Nguyen Dau Nam",
-        src: "./mp3/Uocnguyendaunam.mp3",
-        img: "./IMG/img2.jpeg",
-    },
-    {
-        song: "Jingle Bell",
-        src: "./mp3/JingleBell.mp3",
-        img: "./IMG/img3.jpeg",
-    },
-    {
-        song: "Chuyen Cu Minh Bo Qua",
-        src: "./mp3/chuyencuminhboqua.mp3",
-        img: "./IMG/chuyencuminhboqua.jpeg",
-    },
-    {
-        song: "Em Chao Tet",
-        src: "./mp3/emchaotet.mp3",
-        img: "./IMG/emchaotet.jpeg",
-    },
-    {
-        song: "Lam Gi Phai Hot",
-        src: "./mp3/lamgiphaihot.mp3",
-        img: "./IMG/lamgiphaihot.jpeg",
-    },
-    {
-        song: "Last Christmas",
-        src: "./mp3/lastchrismas.mp3",
-        img: "./IMG/lastchristmas.jpeg",
-    },
-        {
-        song: "Tet Nay Con Se Ve",
-        src: "./mp3/tetnayconseve.mp3",
-        img: "./IMG/tetnayconseve.jpeg",
-    },
-    {
-        song: "Thien Duyen Tien Dinh",
-        src: "./mp3/thienduyentiendinh.mp3",
-        img: "./IMG/thienduyentiendinh.jpeg",
-    },
-    {
-        song: "Trai Tai Gai Sat",
-        src: "./mp3/traitaigaisat.mp3",
-        img: "./IMG/traitaigaisat.jpeg",
-    },
-    {
-        song: "Vang Trang Khoc",
-        src: "./mp3/vangtrangkhoc.mp3",
-        img: "./IMG/vangtrangkhoc.jpeg",
-    },
+  {
+    song: "Doan Xuan Ca",
+    src: "./mp3/DoanXuanCa.mp3",
+    img: "./IMG/img1.jpeg"
+  },
+  {
+    song: "Uoc Nguyen Dau Nam",
+    src: "./mp3/Uocnguyendaunam.mp3",
+    img: "./IMG/img2.jpeg"
+  },
+  {
+    song: "Jingle Bell",
+    src: "./mp3/JingleBell.mp3",
+    img: "./IMG/img3.jpeg"
+  },
+  {
+    song: "Chuyen Cu Minh Bo Qua",
+    src: "./mp3/chuyencuminhboqua.mp3",
+    img: "./IMG/chuyencuminhboqua.jpeg"
+  },
+  {
+    song: "Em Chao Tet",
+    src: "./mp3/emchaotet.mp3",
+    img: "./IMG/emchaotet.jpeg"
+  },
+  {
+    song: "Lam Gi Phai Hot",
+    src: "./mp3/lamgiphaihot.mp3",
+    img: "./IMG/lamgiphaihot.jpeg"
+  },
+  {
+    song: "Last Christmas",
+    src: "./mp3/lastchrismas.mp3",
+    img: "./IMG/lastchristmas.jpeg"
+  },
+  {
+    song: "Tet Nay Con Se Ve",
+    src: "./mp3/tetnayconseve.mp3",
+    img: "./IMG/tetnayconseve.jpeg"
+  },
+  {
+    song: "Thien Duyen Tien Dinh",
+    src: "./mp3/thienduyentiendinh.mp3",
+    img: "./IMG/thienduyentiendinh.jpeg"
+  },
+  {
+    song: "Vang Trang Khoc",
+    src: "./mp3/vangtrangkhoc.mp3",
+    img: "./IMG/vangtrangkhoc.jpeg"
+  },
+  {
+    song: "Trai Tai Gai Sat",
+    src: "./mp3/traitaigaisac.mp3",
+    img: "./IMG/traitaigaisac.jpeg"
+  }
 ];
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
@@ -74,184 +74,184 @@ const btnlike = $(".btn-like");
 let playing = true;
 //! view play music
 function viewPlayMusic() {
-    $(".btn-play i ").classList.remove("fa-play");
-    $(".btn-play i ").classList.add("fa-pause");
-    animateImg.play();
-    audio.play();
-    playing = false;
+  $(".btn-play i ").classList.remove("fa-play");
+  $(".btn-play i ").classList.add("fa-pause");
+  animateImg.play();
+  audio.play();
+  playing = false;
 }
 // ! handel Event Time Now
 audio.addEventListener("timeupdate", handelRealTime);
 function handelRealTime() {
+  const time = (audio.currentTime / audio.duration) * 100;
+  rangeStyle.value = time;
+  if (btnRepeat.matches(".active-footer")) {
     const time = (audio.currentTime / audio.duration) * 100;
-    rangeStyle.value = time;
-    if (btnRepeat.matches(".active-footer")) {
-        const time = (audio.currentTime / audio.duration) * 100;
-        handelRepeatMusic(time);
-    } else if (btnRandom.matches(".active-footer") && time === 100) {
-        const randomIndex = parseInt(Math.random() * listSong.length);
-        handelRandomMusic(randomIndex);
-    } else {
-        handelAutoNext(time);
-    }
+    handelRepeatMusic(time);
+  } else if (btnRandom.matches(".active-footer") && time === 100) {
+    const randomIndex = parseInt(Math.random() * listSong.length);
+    handelRandomMusic(randomIndex);
+  } else {
+    handelAutoNext(time);
+  }
 
-    return time;
+  return time;
 }
 // ! handel auto Next music
 function handelAutoNext(time) {
-    if (time === 100) {
-        handelNextSong(getNowIndex());
-        viewPlayMusic();
-    }
-    getNowIndex();
+  if (time === 100) {
+    handelNextSong(getNowIndex());
+    viewPlayMusic();
+  }
+  getNowIndex();
 }
 
 // ! Handel Event Change
 rangeStyle.addEventListener("change", handeChange);
 function handeChange(e) {
-    audio.currentTime = (audio.duration / 100) * e.target.value;
+  audio.currentTime = (audio.duration / 100) * e.target.value;
 }
 // ! Handel Play Music
 btnPlay.addEventListener("click", handelClickPlay);
 function handelClickPlay(e) {
-    if (playing) {
-        getNowIndex();
-        viewPlayMusic();
-    } else {
-        audio.pause();
-        $(".btn-play i ").classList.remove("fa-pause");
-        $(".btn-play i ").classList.add("fa-play");
-        animateImg.pause();
-        playing = true;
-    }
+  if (playing) {
+    getNowIndex();
+    viewPlayMusic();
+  } else {
+    audio.pause();
+    $(".btn-play i ").classList.remove("fa-pause");
+    $(".btn-play i ").classList.add("fa-play");
+    animateImg.pause();
+    playing = true;
+  }
 }
 //! handel Next Song
 btnNext.addEventListener("click", handelClickNext);
 function handelClickNext(e) {
-    handelNextSong(getNowIndex());
-    viewPlayMusic();
+  handelNextSong(getNowIndex());
+  viewPlayMusic();
 }
 function handelNextSong(index) {
-    index++;
-    if (index >= listSong.length) {
-        index = 0;
-    }
-    songPlaying.textContent = listSong[index].song;
-    imgSong.setAttribute("src", listSong[index].img);
-    audio.setAttribute("src", listSong[index].src);
+  index++;
+  if (index >= listSong.length) {
+    index = 0;
+  }
+  songPlaying.textContent = listSong[index].song;
+  imgSong.setAttribute("src", listSong[index].img);
+  audio.setAttribute("src", listSong[index].src);
 }
 //! handel Prev Song
 btnPrev.addEventListener("click", handelClickPrev);
 function handelClickPrev(e) {
-    handelPrevSong(getNowIndex());
-    viewPlayMusic();
+  handelPrevSong(getNowIndex());
+  viewPlayMusic();
 }
 async function handelPrevSong(index) {
-    index--;
-    if (index < 0) {
-        index = listSong.length - 1;
-    }
-    songPlaying.textContent = listSong[index].song;
-    imgSong.setAttribute("src", listSong[index].img);
-    await audio.setAttribute("src", listSong[index].src);
+  index--;
+  if (index < 0) {
+    index = listSong.length - 1;
+  }
+  songPlaying.textContent = listSong[index].song;
+  imgSong.setAttribute("src", listSong[index].img);
+  await audio.setAttribute("src", listSong[index].src);
 }
 
 // ! handel Get Now Index
 function getNowIndex() {
-    const srcNow = audio.getAttribute("src");
-    const songNow = listSong.filter((value) => {
-        return value.src === srcNow;
-    });
-    let indexNow = listSong.indexOf(songNow[0]);
-    return indexNow;
+  const srcNow = audio.getAttribute("src");
+  const songNow = listSong.filter((value) => {
+    return value.src === srcNow;
+  });
+  let indexNow = listSong.indexOf(songNow[0]);
+  return indexNow;
 }
 // ! handel repeat music
 btnRepeat.addEventListener("click", handelClickRepeat);
 function handelClickRepeat(e) {
-    if (!btnRandom.matches(".active")) handelAnimitionfooter(btnRepeat);
+  if (!btnRandom.matches(".active")) handelAnimitionfooter(btnRepeat);
 }
 function handelRepeatMusic(time) {
-    if (time === 100) {
-        playing = true;
-        handelClickPlay();
-    }
+  if (time === 100) {
+    playing = true;
+    handelClickPlay();
+  }
 }
 // ! handel random music
 btnRandom.addEventListener("click", handelClickRandom);
 function handelClickRandom(e) {
-    if (!btnRepeat.matches(".active")) handelAnimitionfooter(btnRandom);
+  if (!btnRepeat.matches(".active")) handelAnimitionfooter(btnRandom);
 }
 async function handelRandomMusic(index) {
-    songPlaying.textContent = listSong[index].song;
-    imgSong.setAttribute("src", listSong[index].img);
-    await audio.setAttribute("src", listSong[index].src);
-    viewPlayMusic();
+  songPlaying.textContent = listSong[index].song;
+  imgSong.setAttribute("src", listSong[index].img);
+  await audio.setAttribute("src", listSong[index].src);
+  viewPlayMusic();
 }
 // ! handel Animation Img
 const animateImg = imgSong.animate(
-    [
-        {
-            transform: "rotate(360deg)",
-        },
-    ],
+  [
     {
-        duration: 10000,
-        iterations: Infinity,
+      transform: "rotate(360deg)"
     }
+  ],
+  {
+    duration: 10000,
+    iterations: Infinity
+  }
 );
 animateImg.pause();
 listSong.forEach((item) => {
-    const template = `<div class="content-item" data-url="${item.src}" data-img="${item.img}" data-name="${item.song}">
+  const template = `<div class="content-item" data-url="${item.src}" data-img="${item.img}" data-name="${item.song}">
             <img src="${item.img}" alt="" class="item-img" />
             <div class="content-text">
                 <p class="name-song">${item.song}</p>
                 <p class="name-singer">Nguyen Dinh Son</p>
             </div>
         </div>`;
-    contentForm.insertAdjacentHTML("beforeend", template);
+  contentForm.insertAdjacentHTML("beforeend", template);
 });
 // ! show menu
 headerMenu.addEventListener("click", handelShowMenu);
 function handelShowMenu(e) {
-    form.classList.add("active-form");
-    const listSongForm = $$(".content-item");
-    listSongForm.forEach((item) => {
-        item.addEventListener("click", () => {
-            playSongForm(item);
-        });
+  form.classList.add("active-form");
+  const listSongForm = $$(".content-item");
+  listSongForm.forEach((item) => {
+    item.addEventListener("click", () => {
+      playSongForm(item);
     });
+  });
 }
 // !  close menu
 btnClose.addEventListener("click", handelCloseForm);
 function handelCloseForm(e) {
-    form.classList.remove("active-form");
+  form.classList.remove("active-form");
 }
 document.addEventListener("click", (e) => {
-    if (!e.target.matches(".form") && !e.target.matches(".header-menu i")) {
-        form.classList.remove("active-form");
-    }
+  if (!e.target.matches(".form") && !e.target.matches(".header-menu i")) {
+    form.classList.remove("active-form");
+  }
 });
 // ! menu play song
 function playSongForm(song) {
-    const getUrl = song.getAttribute("data-url");
-    const getName = song.getAttribute("data-name");
-    const getSrc = song.getAttribute("data-img");
-    audio.setAttribute("src", getUrl);
-    songPlaying.textContent = getName;
-    imgSong.setAttribute("src", getSrc);
-    viewPlayMusic();
-    handelCloseForm();
+  const getUrl = song.getAttribute("data-url");
+  const getName = song.getAttribute("data-name");
+  const getSrc = song.getAttribute("data-img");
+  audio.setAttribute("src", getUrl);
+  songPlaying.textContent = getName;
+  imgSong.setAttribute("src", getSrc);
+  viewPlayMusic();
+  handelCloseForm();
 }
 // ! handel like
 btnlike.addEventListener("click", (e) => {
-    handelAnimitionfooter(btnlike);
+  handelAnimitionfooter(btnlike);
 });
 function handelAnimitionfooter(el) {
-    if (!el.matches(".active-footer")) {
-        el.classList.add("active-footer");
-        el.classList.remove("unactive-footer");
-    } else {
-        el.classList.remove("active-footer");
-        el.classList.add("unactive-footer");
-    }
+  if (!el.matches(".active-footer")) {
+    el.classList.add("active-footer");
+    el.classList.remove("unactive-footer");
+  } else {
+    el.classList.remove("active-footer");
+    el.classList.add("unactive-footer");
+  }
 }
